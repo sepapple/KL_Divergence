ee8499a9997ab27fe16caad0a65436f5135dd7e6
sepapple <58419339+sepapple@users.noreply.github.com>
2021-05-22 20:19:46 +0900

    adding_JS_divergence, removing_0value

--- KL_JS_Divergence_Sparse.py
+++ KL_JS_Divergence_Sparse.py
@@ -18,13 +18,23 @@ def normalization(x):
 def KLdivergence(p,q):
     # print(p)
     # print(q)
-    # print([b * np.log(a/b) for a,b in zip(p,q)])   
+    counter = 0
+
+    #0が入っていないかをチェック
+    now_zero_location = p==0
+    prev_zero_location = q==0
+    for i in zip(now_zero_location,prev_zero_location):
+        if(i[0] == True or i[1] == True):
+            # print("\n0を発見しました隊長！\n")
+            p = np.delete(p,counter)
+            q = np.delete(q,counter)
+        counter += 1
+
     KL=(np.sum([b * np.log(a/b) for a,b in zip(p,q)]))*(-1)
     return KL
 
 def JSdivergence(p,q):
     pq2 = (p + q) / 2
-    print(pq2)
     kl1 =  KLdivergence(p ,pq2)
     kl2 =  KLdivergence(q ,pq2)
     JS = (kl1 / 2) + (kl2 / 2)
@@ -115,15 +125,22 @@ def main():
     plt.subplot(1,3,1)
     plt.plot(x,show_curr_RawData,color='r')  
     plt.title("Raw Data(Current)") 
+    plt.xlabel("Frame")
+    plt.ylabel("Amptitude")
     plt.subplot(1,3,2)
     plt.plot(x,show_prev_RawData,color='b')  
     plt.title("Raw Data(Previous)") 
+    plt.xlabel("Frame")
+    plt.ylabel("Amptitude")
     plt.subplot(1,3,3)
     plt.title("Raw Data(Combination)") 
     plt.plot(x,show_curr_RawData,color='r')  
     plt.plot(x,show_prev_RawData,color='b')  
-    # plt.tight_layout()
-    # plt.show()
+    plt.xlabel("Quantity")
+    plt.ylabel("Amptitude")
+    
+    plt.tight_layout()
+    plt.show()
     # exit(1)
     
 
@@ -132,6 +149,8 @@ def main():
         show_data = []
         plt.subplot(math.ceil(num/2),4,roop_count*2+1)
         plt.title(str(range_start*100+i*6)+"cm(Current)") 
+        plt.xlabel("Quantity")
+        plt.ylabel("Amptitude")
         for j in range(len(storage)):
             show_data.append(storage[j][i])
         plt.plot(range(1,sample+1),show_data,color='r')  
@@ -139,10 +158,12 @@ def main():
         plt.subplot(math.ceil(num/2),4,roop_count*2+2)
         plt.title(str(range_start*100+i*6)+"cm(Previous)") 
         plt.plot(range(1,sample+1),prev_getData[i],color='b')
+        plt.xlabel("Quantity")
+        plt.ylabel("Amptitude")
         roop_count += 1
 
-    # plt.tight_layout()
-    # plt.show()
+    plt.tight_layout()
+    plt.show()
 
     #データをあわせて表示
     for i in range(num):
@@ -153,12 +174,12 @@ def main():
         plt.plot(range(1,sample+1),show_data,color='r',label="Current Data")  
         plt.title(str(range_start*100+i*6)+"cm(Combination)") 
         plt.plot(range(1,sample+1),prev_getData[i],color='b',label="Previous Data")
-        plt.xlabel("frame")
-        plt.ylabel("amptitude")
+        plt.xlabel("Quantity")
+        plt.ylabel("Amptitude")
         plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left', borderaxespad=0,fontsize='small')
 
-    # plt.tight_layout()
-    # plt.show()
+    plt.tight_layout()
+    plt.show()
 
     #ある距離の時系列データの遷移とそのデータのヒストグラムの表示   
     now_getData = []
@@ -167,6 +188,8 @@ def main():
         # plt.subplot(math.ceil(num/2),2,i+1)
         plt.subplot(math.ceil(num/2),4,i*2+1)
         plt.title(str(range_start*100+i*6)+"cm") 
+        plt.xlabel("Distance") 
+        plt.ylabel("Amptitude") 
         for j in range(len(storage)):
             show_data.append(storage[j][i])
 
@@ -177,11 +200,13 @@ def main():
         plt.subplot(math.ceil(num/2),4,i*2+2)
         plt.hist(show_data,bins=10,density=True,color = "aqua")
         plt.title(str(range_start*100+i*6)+"cm") 
+        plt.xlabel("Class") 
+        plt.ylabel("Frequency") 
         # print("ヒストグラムの度数"+str(hist[0]))
         # print("階級を区切る値"+str(hist[1]))
 
-    # plt.tight_layout()
-    # plt.show()
+    plt.tight_layout()
+    plt.show()
 
     #ndarray型に変換し、保存
     # now_getData = np.array(now_getData)
@@ -189,61 +214,68 @@ def main():
 
     roop_count = 0
     prev_getData = np.loadtxt('sparse.csv')
+    KL_strage = []
+    JS_strage = []
+
     #ヒストグラムの度数からKLを算出
     for i in zip(prev_getData,now_getData):
         plt.subplot(math.ceil(num/2),4,roop_count*2+1)
         plt.title(str(range_start*100+roop_count*6)+"cm(Previous)") 
+        plt.xlabel("Distance") 
+        plt.ylabel("Frequency") 
         now_hist = plt.hist(i[0],bins=10,color = "lime")
-        now_hist = list(now_hist)
         now_hist = normalization(np.array(now_hist[0]))
-        # print(now_hist)
+
         # now_hist_normalization = normalization(np.array(now_hist[0]))
         # now_hist[0] = now_hist_normalization
-        # plt.hist(now_hist)
         # print(now_hist)
 
         plt.subplot(math.ceil(num/2),4,roop_count*2+2)
         plt.title(str(range_start*100+roop_count*6)+"cm(Now)") 
+        plt.xlabel("Distance") 
+        plt.ylabel("Frequency") 
         prev_hist = plt.hist(i[1],bins=10,color = "deepskyblue")
         prev_hist = normalization(np.array(prev_hist[0]))
         # print("now_histの要素の数: "+str(len(now_hist[0])))
         # print("prev_histの要素の数: "+str(len(now_hist[0])))
-        # value = KLdivergence(now_hist,prev_hist)
-        # print(str(range_start*100+roop_count*6)+"cm時のKL_Divergence: "+str(value))
+        KL_value = KLdivergence(now_hist,prev_hist)
+        print(str(range_start*100+roop_count*6)+"cm時のKL_Divergence: "+str(KL_value))
         JS_value = JSdivergence(now_hist,prev_hist)
-        print(JS_value)
-        exit(1)
-        print(str(range_start*100+i*6)+"cm時のJS_Divergence: "+str(JS_value))
+        print(str(range_start*100+roop_count*6)+"cm時のJS_Divergence: "+str(JS_value)+"\n")
+        KL_strage.append(KL_value)
+        JS_strage.append(JS_value)
         roop_count += 1
     
-    # plt.tight_layout()
-    # plt.show()
-
     
-    # p(x)
-    #300個のフレームから距離ごとに平均を表示
-    # ax = plt.subplot(1,1,1)
-    # x = np.arange(range_start*100,range_end*100+1,6)
-    # plt.plot(x,result,label='$p(x)$')  
-    # plt.legend(loc=1,prop={'size': 13})
-    # plt.xlabel('$x[cm]$')
-    # plt.show()
+    plt.tight_layout()
+    plt.show()
+    
+    X = list(range(int(range_start*100),int(range_end*100+1),6))
+    plt.subplot(1,3,1)
+    # plt.plot(range(len(KL_strage)),KL_strage,color='r',marker="o")  
+    plt.plot(X,KL_strage,color='r',marker="o")  
+    plt.title("KL Divergence") 
+    plt.xlabel("Distance") 
+    plt.ylabel("Amptitude") 
 
-# 凡例
+    plt.subplot(1,3,2)
+    plt.plot(X,JS_strage,color='b',marker="o")  
+    plt.title("JS Divergence") 
+    plt.xlabel("Distance") 
+    plt.ylabel("Amptitude") 
 
-# q(x)
-#     qx = plt.subplot(1,2,2)
-#     plt.plot(x,df2,label='$q(x)$')  
-# 
-# # 凡例
-#     plt.legend(loc=1,prop={'size': 13})
-# #plt.xticks(np.arange(0,5000+1,500))
-#     plt.xlabel('$x[cm]$')
-# 
-#     ax.set_title('$KL(p||q)=%.16f$' % KL_U2,fontsize=20)
-#     print(KL_U2)
-# 
-    # plt.tight_layout()
+    plt.subplot(1,3,3)
+    plt.plot(X,KL_strage,color='r',marker="o")  
+    plt.plot(X,JS_strage,color='b',marker="o")  
+    # plt.plot(X,KL_strage,color='r',marker="o",linewidth=0)  
+    # plt.plot(X,JS_strage,color='b',marker="o",linewidth=0)  
+    plt.title("Compare") 
+    plt.xlabel("Distance") 
+    plt.ylabel("Amptitude") 
+    
+    plt.tight_layout()
+    plt.show()
+
     print("Disconnecting...")
     client.disconnect()
 
